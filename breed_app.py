7297314d3f1baac4b5cecd57b133ef995da484b6
yamilart <81629326+yamilart@users.noreply.github.com>
2021-12-16 17:57:56 +0100

    Update breed_app.py

--- breed_app.py
+++ breed_app.py
@@ -49,6 +49,6 @@ if dog_image:
         result = result_prob.argmax(axis=-1)
         le = preprocessing.LabelEncoder()
         result = le.fit_transform(result)
-        return print("I'm ", str(float(round(np.amax(result_prob)*100,2))), '% sure this cute dog is a ', result[0])
+        print("I'm ", str(float(round(np.amax(result_prob)*100,2))), '% sure this cute dog is a ', result[0])
         st.title(print)
         #st.title("I'm ", str(float(round(np.amax(result_prob)*100,2))), '% sure this cute dog is a ', result[0])
