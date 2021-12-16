ffd753e88d2fcb9c773ce2e76f5237821aa97f91
yamilart <81629326+yamilart@users.noreply.github.com>
2021-12-16 20:52:10 +0100

    Update breed_app.py

--- breed_app.py
+++ breed_app.py
@@ -25,10 +25,10 @@ st.set_page_config(
 
 model = load_model('-20-breeds.h5')
 
-breedselection = ['Cocker Spaniel', 'golden_retriever', 'Great Dane', 'siberian_husky', 
+breedselection = ['Cocker Spaniel', 'golden_retriever', 'Great Dane', 'Chihuahua', 
                                                 'Chow', 'french_bulldog', 'rottweiler', 'Dachshund', 
                                                 'pekinese', 'doberman', 'boxer', 'Weimaraner', 
-                                                'Toy Terrier', 'beagle', 'chihuahua', 'Samoyed', 
+                                                'Toy Terrier', 'beagle', 'Siberian Husky', 'Samoyed', 
                                                 'Labrador Retriever', 'collie', 'bloodhound', 'yorkshire_terrier']
 
 st.title("Who let the dogs out?")
